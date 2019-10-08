5295def1d1d7d0bafe27bfdfe687839c1d30256f
Andrew Trigg <andrewt51@hotmail.com>
2019-10-08 22:19:37 +0100

    Initial dev setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new FaviconsWebpackPlugin("./static/logo.png")
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000
+  }
+};
