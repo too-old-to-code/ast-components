c20348fcb1dbe14c1c362e074e2979239410e38d
Andrew Trigg <andrewt51@hotmail.com>
2019-10-17 21:00:47 +0100

    default export

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { PrimaryButton } from './atoms/buttons';
-export default PrimaryButton;
-// export { PrimaryButton } from './atoms/buttons';
-// class MyApp extends Component {
-//   render() {
-//     return <div>Hello World</div>;
-//   }
-// }
-//
-// export const PrimaryButton
-// export { PrimaryButton, SecondaryButton };
 
-// ReactDOM.render(<MyApp />, document.getElementById('app'));
+// import { PrimaryButton } from './atoms/buttons';
+// export default PrimaryButton;
+
+export * from './atoms/buttons';
