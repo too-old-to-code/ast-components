a108023ba3fd43b89690fc088ed6d134bb00d906
Andrew Trigg <andrewt51@hotmail.com>
2019-10-17 20:27:25 +0100

    Build and allow dist folder to be stored in git

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { PrimaryButton } from './atoms/buttons';
 
-class MyApp extends Component {
-  render() {
-    return <div>Hello World</div>;
-  }
-}
+// class MyApp extends Component {
+//   render() {
+//     return <div>Hello World</div>;
+//   }
+// }
+//
+// export const PrimaryButton
+export default PrimaryButton;
 
-ReactDOM.render(<MyApp />, document.getElementById('app'));
+// ReactDOM.render(<MyApp />, document.getElementById('app'));
